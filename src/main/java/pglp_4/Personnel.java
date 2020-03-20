3a10413620bffd5e977a8674fc4f4b31dcc150f7
Hakim OUEDRAOGO <ahakim.ouedraogo@gmail.com>
2020-03-20 14:24:02 +0100

    Class Personnel

---
+++ src/main/java/pglp_4/Personnel.java
@@ -0,0 +1,15 @@
+package pglp_4;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class Personnel {
+    private String nom;
+    private String prenom;
+    private String fonction;
+    private LocalDate ddn;
+    private Map<String, Integer> telephones;
+
+   
+}
